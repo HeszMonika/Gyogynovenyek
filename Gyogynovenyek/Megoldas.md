05835b4c2249c9ed3633b0d57728047717c6d927
HeszMonika <heszmoni2001@gmail.com>
2020-10-12 11:40:54 +0100

    Kész feladat.

--- Gyogynovenyek/Megoldas.md
+++ Gyogynovenyek/Megoldas.md
@@ -23,7 +23,7 @@
   - Konstruktorban számoljuk!
   - számolása: Vég - Kezdet, HA Vég > Kezdet
   - Vég > Kezdet --> Vég - Kezdet + 1
-  - Egyébként --> 12 - Vég + Kezdet + 1
+  - Egyébként --> 12 - Kezdet + Vég + 1
 
 ## Adatok beolvasása és eltárolása
 - Listában tároljuk az objektum páldányokat
@@ -39,10 +39,27 @@
 ## 2. feladat:
 - Milyen gyűjthető részek vannak?
 - Hány darab van ezekből?
+- Dictionary<string, int>
+- Milyen gyűjthető részek vannak?
+  - foreach Novenyek
+  - Ha a Resz nem kulcs --> akkor adjuk hozzá 0-val
+- Hány darab van ezekből?
+  - foreach  Novenyek
+  - Ja "Resz" == Key --> Value + 1
 
 ## 3. feladat:
 - Maximum keresés --> Időtartam alapján
+  - maximum változó = 0
+  - foreach Novenyek
+  - Ha max < Idotartam --> max = Idotartam
 - Növény(ek), amelyeket a legtöbb ideig szedtek
+  - foreach Novenyek
+  - Kiírni, ha Idotartam == max
 
 ## 4. feladat:
 - Időtartam átlag számítása
+  - szum változó = 0 (double típus)
+  - atlag változó
+  - foreach Novenyek
+  - szum = szum + Idotartam
+  - atlag = szum / Count
--- Gyogynovenyek/Program.cs
+++ Gyogynovenyek/Program.cs
@@ -3,13 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Gyogynovenyek
 {
     class Program
     {
+        static List<Gyogynovenykek> Novenyek = new List<Gyogynovenykek>();
+
+
+        static Dictionary<string, int> Reszek = new Dictionary<string, int>();
+
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("noveny.csv");
+            while (!sr.EndOfStream)
+            {
+                Novenyek.Add(new Gyogynovenykek(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+
+        static void elsoFeladat()
+        {
+            int db = 0;
+            for (int i = 0; i < Novenyek.Count; i++)
+            {
+                db++;
+            }
+            Console.WriteLine("\n1. feladat: Növények száma: {0}", db);
+
+
+            //Másik megoldás: int a = Novenyek.Count();
+        }
+
+
+        static void masodikFeladat()
+        {
+            Console.WriteLine("\n2. feladat: Gyűjthető részek:");
+            foreach (var n in Novenyek)
+            {
+                if (!Reszek.ContainsKey(n.Resz))
+                {
+                    Reszek.Add(n.Resz, 0);
+                }
+            }
+
+            foreach (var n in Novenyek)
+            {
+                Reszek[n.Resz]++;
+            }
+
+            foreach (var r in Reszek)
+            {
+                Console.WriteLine($"{r.Key}: {r.Value}");
+            }
+        }
+
+
+        static void harmadikFeladat()
+        {
+            int max = 0;
+            foreach (var n in Novenyek)
+            {
+                if (max < n.Idotartam)
+                {
+                    max = n.Idotartam;
+                }
+            }
+
+            foreach (var n in Novenyek)
+            {
+                if (max == n.Idotartam)
+                {
+                    Console.WriteLine("\n3. feladat: A legtöbb idő, amíg gyűjthető: {0} Növények: {1}", max, n.Nev);
+                }
+            }
+        }
+
+
+        static void negyedikFeladat()
+        {
+            double szum = 0;
+            double atlag = 0;
+            foreach (var n in Novenyek)
+            {
+                szum = szum + n.Idotartam;
+            }
+            atlag = szum / Novenyek.Count();
+            Console.WriteLine("\n4. feladat: Átlagos gyűjthető időtartam: {0}", atlag);
+        }
+
+
         static void Main(string[] args)
         {
+            Beolvasas();
+            //foreach (var n in Novenyek)
+            //{
+            //    Console.WriteLine($"{n.Nev} - {n.Resz} - {n.Kezd} - {n.Veg} - {n.Idotartam}");
+            //}
+
+            elsoFeladat();
+            masodikFeladat();
+            harmadikFeladat();
+            negyedikFeladat();
+
             Console.ReadKey();
         }
     }
